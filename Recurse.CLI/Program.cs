fab842a382f8ba6d522b89ff685716747beea3a7
Aly Ashour <alyashour1@gmail.com>
2024-12-11 02:48:28 -0500

    Initial Commit

---
+++ Recurse.CLI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.CommandLine;
+
+namespace Recurse.CLI;
+
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        // root commands are run when the app is called without any args
+        var rootCommand = new RootCommand("Simple command-line app");
+
+        rootCommand.SetHandler(() =>
+        {
+            Console.WriteLine("Hello, World!");
+        });
+
+        await rootCommand.InvokeAsync(args);
+    }
+}
---
+++ Recurse.Core/README.md
@@ -0,0 +1,6 @@
+# Recurse
+
+Recurse is software that let's you manage large-scale organizational operations and development written in C#.
+I was inspired to write it after starting my club and needing a way to communicate with my executives and give them a way to communicate with their leads recursively.
+
+Still a WIP! Stay tuned in for first release.
